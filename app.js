b43ae1b0ab3bf17eb3f2eb8f37e55cfbe4248cf2
RoufDs <four.3687zero@gmail.com>
2025-03-30 20:36:04 +0700

    route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json()) // Middleware to parse JSON data from request body
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (_, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -16,9 +16,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   })
-})
+}
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1 // convert string to number
   const tour = tours.find(x => x.id === id)
 
@@ -36,9 +36,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour: tour,
     },
   })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = Object.assign({
     id: newId
@@ -55,9 +55,9 @@ app.post('/api/v1/tours', (req, res) => {
       },
     })
   })
-})
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1 // convert string to number
   const tour = tours.find(x => x.id === id)
 
@@ -75,9 +75,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...>'
     },
   })
-})
+}
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const id = req.params.id * 1 // convert string to number
   const tour = tours.find(x => x.id === id)
 
@@ -93,7 +93,24 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   })
-})
+}
+
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour)
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
 
 const port = process.env.PORT;
 
