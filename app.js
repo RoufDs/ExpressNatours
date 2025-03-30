428526af08ad1e645cc2ec79734ff221e0612abc
RoufDs <four.3687zero@gmail.com>
2025-03-30 15:17:52 +0700

    mockup for test

--- app.js
+++ app.js
@@ -1,14 +1,98 @@
 require('dotenv').config()
+const fs = require('fs')
 const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-      message: 'Hello from the server!',
-    });
+app.use(express.json()) // Middleware to parse JSON data from request body
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  })
+})
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1 // convert string to number
+  const tour = tours.find(x => x.id === id)
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    })
+    return
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({
+    id: newId
+  }, req.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      message: 'New tour added successfully',
+      data: {
+        tour: newTour,
+      },
+    })
+  })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1 // convert string to number
+  const tour = tours.find(x => x.id === id)
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    })
+    return
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    },
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1 // convert string to number
+  const tour = tours.find(x => x.id === id)
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    })
+    return
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  })
 })
 
 const port = process.env.PORT;
