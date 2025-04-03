977c409e1668cb7f31961fd5026b016deec10f25
RoufDs <four.3687zero@gmail.com>
2025-04-03 21:40:33 +0700

    tourModel

--- server.js
+++ server.js
@@ -14,25 +14,6 @@ mongoose.connect(DB)
     }
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-})
-const Tour = mongoose.model('Tour', tourSchema)
-
 const port = process.env.PORT;
 
 app.listen(port, () => {
