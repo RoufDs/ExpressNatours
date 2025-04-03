21f92c498a4b80980968c0791d169cb6d7ad5f04
RoufDs <four.3687zero@gmail.com>
2025-04-03 21:00:03 +0700

    mongoose (connect to atlas)

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 require('dotenv').config()
-
+const mongoose = require('mongoose')
 const app = require("./app");
+
+const DB = process.env.DB_SERVER.replace('<PASSWORD>', process.env.DB_PASSWORD)
+mongoose.connect(DB)
+  .then(() => {
+    if (process.env.NODE_ENV.trim() === 'development') {
+      console.log('Connected to MongoDB!');
+    }
+  }).catch(err => {
+    if (process.env.NODE_ENV.trim() === 'development') {
+      console.error('Connection error:', err);
+    }
+  });
+
 
 const port = process.env.PORT;
 
