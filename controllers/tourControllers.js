dee81da1620de534d993be4d3ef5c3eaf145ed0d
RoufDs <four.3687zero@gmail.com>
2025-04-03 22:00:54 +0700

    create tour using mongoose

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,66 +1,32 @@
-const fs = require('fs')
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
-
-exports.checkId = (req, res, next, val) => {
-  const id = req.params.id * 1 // convert string to number
-  const tour = tours.find(x => x.id === id)
-
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Tour not found',
-    })
-    return
-  }
-
-  next() // Call the next middleware or route handler
-}
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    })
-    return
-  }
-
-  next() // Call the next middleware or route handler
-}
+const Tour = require('../models/tourModel')
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours: tours,
+    // },
   })
 }
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1 // convert string to number
-  const tour = tours.find(x => x.id === id)
+  // const tour = tours.find(x => x.id === id)
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  })
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: tour,
+  //   },
+  // })
 }
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({
-    id: newId
-  }, req.body)
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create({ ...req.body })
 
-  tours.push(newTour)
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
     res.status(201).json({
       status: 'success',
       message: 'New tour added successfully',
@@ -68,12 +34,17 @@ exports.createTour = (req, res) => {
         tour: newTour,
       },
     })
-  })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+      error: error.message,
+    })
+  }
 }
 
 exports.updateTour = (req, res) => {
   const id = req.params.id * 1 // convert string to number
-  const tour = tours.find(x => x.id === id)
 
   res.status(200).json({
     status: 'success',
