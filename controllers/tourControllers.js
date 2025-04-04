d647998371c5d8dd979960ce965f78e26c6bdbf3
RoufDs <four.3687zero@gmail.com>
2025-04-04 09:48:57 +0700

    delete tour using mongoose

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -88,9 +88,25 @@ exports.updateTour = async (req, res) => {
   }
 }
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  })
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id)
+    if (!tour) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Tour not found',
+      })
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error
+    })
+
+  }
 }
